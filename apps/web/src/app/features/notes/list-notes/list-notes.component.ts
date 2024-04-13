8d02efa9dab4dcc63e8d532c72cf151d6958b843
aimadjaouhar1 <aimadjaouhar@icloud.com>
2024-04-13 12:23:31 +0200

    refactor: fix design of notes & list-notes

--- apps/web/src/app/features/notes/list-notes/list-notes.component.ts
+++ apps/web/src/app/features/notes/list-notes/list-notes.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { INote } from '@notes/shared-lib/interfaces/note.interface';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-list-notes',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './list-notes.component.html',
   styleUrl: './list-notes.component.scss',
 })
