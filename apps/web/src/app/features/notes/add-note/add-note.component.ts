bd5560eabb0838ba742b2652d9d0f52cd683384a
aimadjaouhar1 <aimadjaouhar@icloud.com>
2024-04-13 08:08:40 +0200

    feat: add note form & validation, emit the form value to parent component

--- apps/web/src/app/features/notes/add-note/add-note.component.ts
+++ apps/web/src/app/features/notes/add-note/add-note.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { INote } from '@notes/shared-lib/interfaces/note.interface';
+
 
 @Component({
   selector: 'app-add-note',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule, ReactiveFormsModule],
   templateUrl: './add-note.component.html',
   styleUrl: './add-note.component.scss',
 })
-export class AddNoteComponent {}
+export class AddNoteComponent {
+  @Input({required: true}) tags!: string[];
+  @Output() add = new EventEmitter<Omit<INote, 'id' | 'status'>>();
+
+  private readonly formBuilder = inject(FormBuilder);
+
+  form = this.formBuilder.group({
+    title: [''],
+    description: [''],
+    tags: [[]]
+  });
+
+  get controls() {
+    return this.form?.controls;
+  }
+
+  isSubmitted = false;
+
+  onSubmit() {
+    this.isSubmitted = true;
+    
+    if(this.form.valid) {
+      const {title, tags, description} = this.form.getRawValue();
+
+      this.add.emit({title: title!, tags: tags ?? [], description: description ?? ''})
+    }
+  }
+
+}
--- apps/web/src/app/features/notes/notes.component.ts
+++ apps/web/src/app/features/notes/notes.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchNotesComponent } from './search-notes/search-notes.component';
+import { AddNoteComponent } from './add-note/add-note.component';
 
 @Component({
   selector: 'app-notes',
   standalone: true,
-  imports: [CommonModule, SearchNotesComponent],
+  imports: [CommonModule, SearchNotesComponent, AddNoteComponent],
   templateUrl: './notes.component.html',
   styleUrl: './notes.component.scss',
 })
