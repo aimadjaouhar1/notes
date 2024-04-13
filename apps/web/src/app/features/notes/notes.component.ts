c610fb5ead103b50356c94a9403204680109f1e9
aimadjaouhar1 <aimadjaouhar@icloud.com>
2024-04-13 06:49:58 +0200

    feat: emit search input value to parent component

--- apps/web/src/app/features/notes/notes.component.ts
+++ apps/web/src/app/features/notes/notes.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SearchNotesComponent } from './search-notes/search-notes.component';
 
 @Component({
   selector: 'app-notes',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SearchNotesComponent],
   templateUrl: './notes.component.html',
   styleUrl: './notes.component.scss',
 })
--- apps/web/src/app/features/notes/search-notes/search-notes.component.ts
+++ apps/web/src/app/features/notes/search-notes/search-notes.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-notes',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule, FormsModule],
   templateUrl: './search-notes.component.html',
   styleUrl: './search-notes.component.scss',
 })
-export class SearchNotesComponent {}
+export class SearchNotesComponent {
+  @Output() search = new EventEmitter<string>();
+
+  searchQuery: string = '';
+
+  onSubmit = () => this.search.emit(this.searchQuery);
+
+}
