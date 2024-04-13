fd438818c6b030077070c56bbaf9fe4e4f385f6b
aimadjaouhar1 <aimadjaouhar@icloud.com>
2024-04-13 14:45:31 +0200

    feat(api): database configuration

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { NoteController } from './controllers/note.controller';
 import { NoteService } from './services/note.service';
 
+const entities = [];
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432'),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [...entities],
+      synchronize: Boolean(process.env.DB_SYNC || false), 
+      logging: Boolean(process.env.DB_LOGGING || false)
+    }),
+  ],
   controllers: [NoteController],
   providers: [NoteService],
 })
