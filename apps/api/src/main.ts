5a3835babc1190ba418e2abbb08aac7a507cfd1a
aimadjaouhar1 <aimadjaouhar@icloud.com>
2024-04-13 15:25:08 +0200

    feat(api): use GlobalExceptionFilter & ValidationPipe Globally

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,17 +3,24 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { GlobalExceptionFilter } from './app/_core/exceptions/global-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new GlobalExceptionFilter());
+
+
   await app.listen(port);
+  
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
