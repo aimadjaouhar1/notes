9c553e84ec66182a99b64e931d8a612ad3d4be79
aimadjaouhar1 <aimadjaouhar@icloud.com>
2024-04-13 09:02:30 +0200

    feat: create NoteHttp

---
+++ apps/web/src/app/_core/http/note.http.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable, inject } from "@angular/core";
+import { INote } from "@notes/shared-lib/interfaces/note.interface";
+import { environment } from "@notes/web/environments/environment";
+import { Observable } from "rxjs";
+
+@Injectable({
+    providedIn: 'root',
+})
+export class NoteHttp {
+    private readonly url = `${environment.baseApi}/notes`;
+
+    private readonly http = inject(HttpClient);
+
+    addNote = (note: Omit<INote, 'id' | 'status'>): Observable<INote> => this.http.post<INote>(`${this.url}`, note);
+     
+}
