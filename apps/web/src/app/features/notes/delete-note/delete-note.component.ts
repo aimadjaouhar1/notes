b8496e866aac07a0f50413bbee5d24f5b62981aa
aimadjaouhar1 <aimadjaouhar@icloud.com>
2024-04-13 14:17:24 +0200

    feat:  delete notes modal

--- apps/web/src/app/features/notes/delete-note/delete-note.component.ts
+++ apps/web/src/app/features/notes/delete-note/delete-note.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { INote } from '@notes/shared-lib/interfaces/note.interface';
+import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-delete-note',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './delete-note.component.html',
   styleUrl: './delete-note.component.scss',
 })
-export class DeleteNoteComponent {}
+export class DeleteNoteComponent {
+  @Input({required: true}) note!: INote;
+  @Input({required: true}) modalRef!: NgbModalRef;
+  @Output() confirm = new EventEmitter<INote>();
+}
