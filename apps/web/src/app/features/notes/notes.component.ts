7ba7ec5d3a94bbee29cbbaecbae78fd22a8ec6cb
aimadjaouhar1 <aimadjaouhar@icloud.com>
2024-04-13 17:56:16 +0200

    feat: Added conditional rendering based on  status success, loading, error

--- apps/web/src/app/features/notes/notes.component.ts
+++ apps/web/src/app/features/notes/notes.component.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SearchNotesComponent } from './search-notes/search-notes.component';
 import { AddNoteComponent } from './add-note/add-note.component';
 import { ListNotesComponent } from './list-notes/list-notes.component';
-import { NgbModal, NgbModalModule, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 import { INote } from '@notes/shared-lib/interfaces/note.interface';
 import { TranslateModule } from '@ngx-translate/core';
 import { Store } from '@ngrx/store';
-import { NotesState, selectGetNotes, getNotes } from '@notes/web/app/store/notes';
+import { NotesState, selectGetNotes, statusSelector, NotesStateStatus, errorMessageSelector } from '@notes/web/app/store/notes';
 import * as NotesActions from '@notes/web/app/store/notes/notes.actions';
 import { DeleteNoteComponent } from './delete-note/delete-note.component';
 
@@ -35,6 +35,10 @@ export class NotesComponent {
   selectedNote!: INote;
 
   notes$ = this.store.select(selectGetNotes);
+  status$ = this.store.select(statusSelector);
+  errorMessage$ = this.store.select(errorMessageSelector);
+
+  NotesStateStatus = NotesStateStatus;
 
   constructor() {
     this.store.dispatch(NotesActions.getNotes());
