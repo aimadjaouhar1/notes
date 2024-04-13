7081a5ae8d715fe605e18f82a3eafbff5cc7bebe
aimadjaouhar1 <aimadjaouhar@icloud.com>
2024-04-13 10:36:48 +0200

    feat: add getNotes actions

--- apps/web/src/app/store/notes/notes.actions.ts
+++ apps/web/src/app/store/notes/notes.actions.ts
@@ -4,3 +4,7 @@ import { INote } from "@notes/shared-lib/interfaces/note.interface";
 export const addNote = createAction('[Note] Add Note', props<{ note: Omit<INote, 'id' | 'status'> }>());
 export const addNoteSuccess = createAction('[Note] Add Note Success', props<{ note: INote }>());
 export const addNoteFailure = createAction('[Note] Add Note Failure');
+
+export const getNotes = createAction('[Note] Get Notes');
+export const getNotesSuccess = createAction('[Note] Get Notes Success', props<{ notes: INote[] }>());
+export const getNoteFailure = createAction('[Note] Get Notes Failure', props<{ errorMessage: string }>());
