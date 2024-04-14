b59c17b4cd8a3b6b7f9c300cc6a37ac428148c53
aimadjaouhar1 <aimadjaouhar@icloud.com>
2024-04-14 20:07:58 +0200

    scripts: add shell script to automate the process of versioning, building and pushing docker images, and generate a docker-compose.prod file for a specific version

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+read -p "Please enter the version number (e.g., 0.1.0): " version_number
+
+if ! [[ $version_number =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "Error: Invalid version number format. Please use the format 0.0.0"
+    exit 1
+fi
+
+echo "Removing dist directory..."
+rm -R dist
+
+echo "Building web project..."
+npx nx build web --skip-nx-cache 
+
+echo "Building api project..."
+npx nx build api --skip-nx-cache 
+
+echo "Building Docker image for web..."
+docker build -t "aimadjaouhar/notes:web-$version_number" -f docker/web.Dockerfile . --no-cache
+
+echo "Building Docker image for api..."
+docker build -t "aimadjaouhar/notes:api-$version_number" -f docker/api.Dockerfile . --no-cache
+
+echo "Pushing Docker image web..."
+docker push "aimadjaouhar/notes:web-$version_number"
+
+echo "Pushing Docker image api..."
+docker push "aimadjaouhar/notes:api-$version_number"
+
+sed "s/\${version}/$version_number/g" docker-compose.prod.example.yml > tmp && mv tmp docker-compose.prod.yml
