5437911d7804a32d93f609dbedd94a8f69626c90
aimadjaouhar1 <aimadjaouhar@icloud.com>
2024-04-14 08:28:08 +0200

    feat: page not found

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -1,8 +1,19 @@
 import { Route } from '@angular/router';
+import { NotFoundComponent } from './_shared/components/not-found/not-found.component';
 
 export const appRoutes: Route[] = [
     {
         path: '',
         loadComponent: () => import('@notes/web/app/features/notes/notes.component').then(c => c.NotesComponent)
+    },
+    { 
+        path: '', 
+        redirectTo: '', 
+        pathMatch: 'full' 
+    },
+    { 
+        path: '**', 
+        pathMatch: 'full',  
+        component: NotFoundComponent
     }
 ];
